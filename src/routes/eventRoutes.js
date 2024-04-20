cb57ad92358391476069e7764730e8e85246ea34
Giovane Souza <developergiovanesouza@gmail.com>
2024-04-19 22:44:14 -0300

    feat: estrutura base

---
+++ src/routes/eventRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+
+const eventRoutes = Router();
+
+
+
+export { eventRoutes };
---
+++ src/routes/localRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+
+const localRoutes = Router();
+
+
+
+export { localRoutes };
