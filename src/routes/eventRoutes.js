c01a128c5be0d15d1f7fb51b6bdfcdfc9b3cea4c
Giovane Souza <developergiovanesouza@gmail.com>
2024-04-22 20:45:27 -0300

    feat: validação (middleware)

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { EventCrontroller } from "../controllers/EventController.js"
+import { EventCrontroller } from "../controllers/EventController.js";
+import { Validation } from "../middleware/validationMiddleware.js";
 
 const eventRoutes = Router();
 const eventController = new EventCrontroller();
+const validation = new Validation();
 
 eventRoutes.get("/events", eventController.findAllEvents);
-eventRoutes.get("/event/:id", eventController.findEventById);
-eventRoutes.post("/event", eventController.createEvent);
-eventRoutes.put("/event/:id", eventController.updateEvent);
-eventRoutes.delete("/event/:id", eventController.deleteEvent);
+eventRoutes.get("/event/:id", validation.validateIdParameter, eventController.findEventById);
+eventRoutes.post("/event", validation.valitadeEventData, eventController.createEvent);
+eventRoutes.put("/event/:id", validation.validateIdParameter, validation.valitadeEventData, eventController.updateEvent);
+eventRoutes.delete("/event/:id", validation.validateIdParameter, eventController.deleteEvent);
 
 
 export { eventRoutes };
