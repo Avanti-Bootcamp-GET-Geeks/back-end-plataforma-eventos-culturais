05a275b53992e590bfcd8f4803fac4ff32ddc00b
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-04-23 01:41:27 -0300

    feat: crud categorias

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { CategoryController } from '../controllers/CategoryController';
+import { CategoryController } from '../controllers/CategoryController.js';
 
 const categoryRoutes = Router();
 const categoryController = new CategoryController();
 
-categoryRoutes.get('/category/:id', categoryController.createCategory);
+categoryRoutes.get('/allCategories', categoryController.findAllCategories);
 categoryRoutes.post('/category', categoryController.createCategory);
+
+categoryRoutes.get('/category/:id', categoryController.findCategoryById);
+categoryRoutes.put('/category/:id', categoryController.updateCategory);
+categoryRoutes.delete('/category/:id', categoryController.deleteCategory);
 
 export { categoryRoutes };
