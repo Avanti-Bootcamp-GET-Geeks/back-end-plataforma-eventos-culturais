a230d2972b7a9f2f5f315c39d6f005bf3d065463
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-04-23 19:17:45 -0300

    fix: atualizadas as modificações de padrão para rotas

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -4,7 +4,7 @@ import { CategoryController } from '../controllers/CategoryController.js';
 const categoryRoutes = Router();
 const categoryController = new CategoryController();
 
-categoryRoutes.get('/allCategories', categoryController.findAllCategories);
+categoryRoutes.get('/categories', categoryController.findAllCategories);
 categoryRoutes.post('/category', categoryController.createCategory);
 
 categoryRoutes.get('/category/:id', categoryController.findCategoryById);
