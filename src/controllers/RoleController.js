1e26f7d9a96fe8cfc3164eae8f47091a5a34d01c
Giovane Souza <developergiovanesouza@gmail.com>
2024-04-26 21:23:39 -0300

    feat: crud cargos

---
+++ src/controllers/RoleController.js
@@ -0,0 +1,78 @@
+import { prismaClient } from "../database/PrismaClient.js";
+
+const findById = async (id) => await prismaClient.cargos.findUnique({ where: { id } });
+
+export class RoleController {
+  async findAllRoles(req, res) {
+    try {
+      const roles = await prismaClient.cargos.findMany();
+      res.status(200).json(roles);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: 'Erro ao buscar cargos.' });
+    }
+  };
+
+  async findRoleById(req, res) {
+    const { id } = req.params;
+    try {
+
+      const role = await findById(id);
+
+      if (!role) {
+        return res.status(404).json({ error: 'Cargo não encontrado.' });
+      };
+
+      res.status(200).json(role);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: 'Erro ao buscar cargo.' });
+    }
+  };
+
+  async createRole(req, res) {
+    try {
+      res.status(201).json(await prismaClient.cargos.create({data: { nome: req.body.nome }}));
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: 'Erro ao criar cargo.' });
+    }
+  };
+
+  async updateRole(req, res) {
+    const { id } = req.params;
+
+    try {
+      const roleFound = await findById(id);
+
+      if (!roleFound) {
+        return res.status(404).json({ error: 'Cargo não encontrado.' });
+      };
+
+      res.status(200).json(await prismaClient.cargos.update({where: { id }, data: { nome: req.body.nome }}));  
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Erro ao atualizar cargo.' });
+    }
+  };
+
+  async deleteRole(req, res) {
+    const { id } = req.params;
+    try {
+      const roleFound = await findById(id);
+
+      if (!roleFound) {
+        return res.status(404).json({ error: 'Cargo não encontrado.' });
+      };
+
+      await prismaClient.cargos.delete({ where: { id } });
+
+      res.status(204).send();
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ error: 'Erro ao excluir cargo.' });
+    }
+  };
+}
